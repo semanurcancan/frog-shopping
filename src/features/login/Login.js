08425f8fbd3ea405c1c26830c738b4dbc69469df
semanur c <semanurakoz90@gmail.com>
2022-09-26 23:43:45 +0300

    login fixed

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -26,11 +26,12 @@ export function Login() {
                 <div className='flex flex-col justify-center bg-green-100'>
                     <a className=' text-xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-violet-500'>ADMIN LOGIN</a>
                     <ul>
-                        <li><input placeholder='Full Name' className='text-sm focus: outline-none focus:outline-gray-200' type="text" onKeyDown={handleKeyDown} ref={name} /></li>
-                        <li><input placeholder='Password' className='text-sm focus: outline-none focus:outline-gray-200' type="password" onKeyDown={handleKeyDown} ref={password} /></li>
+                        <li><input placeholder='test' className='text-sm focus: outline-none focus:outline-gray-200' type="text" onKeyDown={handleKeyDown} ref={name} /></li>
+                        <li><input placeholder='test' className='text-sm focus: outline-none focus:outline-gray-200' type="password" onKeyDown={handleKeyDown} ref={password} /></li>
                         <br></br>
                         <li><button className='bg-white  text-gray-800 font-semibold py-1 px-2 border border-gray-200 focus:outline-gray-200 rounded shadow text-xl bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-violet-500'
                             onClick={() => checkLogin()}>Submit</button></li>
+
                     </ul>
                 </div>
                 <div className='  justify-center items-center	bg-green-200 grid content-center	'>
